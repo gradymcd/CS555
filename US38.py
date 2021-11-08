603531c3ebcd41f75cab3e65e39d48e5ffeb7ba6
garyung171 <garyung17@gmail.com>
2021-11-08 16:19:45 -0500

    fixed some bugs with us38

--- US38.py
+++ US38.py
@@ -5,20 +5,26 @@ from dateutil.relativedelta import relativedelta
 # US38
 def checkUpcomingBirthday(datestring):
     birthdate = datetime.strptime(datestring, "%Y-%m-%d")
+    birthdate = date(datetime.now().year, birthdate.month, birthdate.day)
     d = relativedelta(birthdate, datetime.now()).days
     m = relativedelta(birthdate, datetime.now()).months
-    y = relativedelta(birthdate, datetime.now()).years
-    if d <= 30 and m == 0 and y == 0:
+    if 0 <= d <= 30 and m == 0:
         return True
     return False
 
 
 class TestUS38(unittest.TestCase):
-    def test_true(self):
+    def test_true1(self):
         self.assertEqual(checkUpcomingBirthday("2021-12-02"), True)
 
-    def test_false(self):
-        self.assertEqual(checkUpcomingBirthday("2022-11-11"), False)
+    def test_true2(self):
+        self.assertEqual(checkUpcomingBirthday("2020-12-02"), True)
+
+    def test_false1(self):
+        self.assertEqual(checkUpcomingBirthday("2021-11-07"), False)
+
+    def test_false2(self):
+        self.assertEqual(checkUpcomingBirthday("2021-12-15"), False)
 
 if __name__ == '__main__':
     unittest.main()
