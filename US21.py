6ca84cbf6404637107d08b7f775b4ef613e24c97
garyung171 <garyung17@gmail.com>
2021-12-02 18:21:22 -0500

    tests added

--- US21.py
+++ US21.py
@@ -1,13 +1,85 @@
+import unittest
 #US 21: Correct gender for role
 
 def corrGen(indis, fams):
+    valid = True
     for key, val in fams.items():
         # assume we have both husb and wife all the time
         husbId = val['Husband ID']
         wifeId = val['Wife ID']
         if indis[husbId]['Gender'] != 'M':
             print('Error: US21: Individual {} is not the correct gender M in family {}'.format(husbId, key))
+            valid = False
         if indis[wifeId]['Gender'] != 'F':
             print('Error: US21: Individual {} is not the correct gender F in family {}'.format(wifeId, key))
+            valid = False
 
-    return
+    return valid
+
+class TestUS31(unittest.TestCase):
+    def test1(self):
+        indis = {
+            'I1': {
+                'Name': 'I1',
+                'Gender': 'M'
+            },
+            'I2': {
+                'Name': 'I2',
+                'Gender': 'F'
+            },
+            'I3': {
+                'Name': 'I2',
+                'Gender': 'M'
+            }
+        }
+        fams = {
+            'F1': {
+                'Husband ID': 'I2',
+                'Wife ID': 'I3',
+                'Children': ['I1']
+            }}
+        self.assertFalse(corrGen(indis, fams))
+
+    def test2(self):
+        indis = {
+            'I1': {
+                'Name': 'I1',
+                'Gender': 'M'
+            },
+            'I2': {
+                'Name': 'I2',
+                'Gender': 'F'
+            },
+            'I3': {
+                'Name': 'I2',
+                'Gender': 'M'
+            }
+        }
+        fams = {
+            'F1': {
+                'Husband ID': 'I1',
+                'Wife ID': 'I2',
+                'Children': []
+            }}
+        self.assertTrue(corrGen(indis, fams))
+
+    def test3(self):
+        indis = {
+            'I1': {
+                'Name': 'I1',
+                'Gender': 'M'
+            },
+            'I2': {
+                'Name': 'I2',
+                'Gender': 'F'
+            },
+            'I3': {
+                'Name': 'I2',
+                'Gender': 'M'
+            }
+        }
+        fams = {}
+        self.assertTrue(corrGen(indis, fams))
+
+if __name__ == '__main__':
+    unittest.main()
--- US31.py
+++ US31.py
@@ -1,3 +1,4 @@
+import unittest
 #List living single
 
 def listLivingSingle(indis):
@@ -7,3 +8,64 @@ def listLivingSingle(indis):
             lst.append(indis[person]['Name'])
 
     return lst
+
+class TestUS31(unittest.TestCase):
+    def test1(self):
+        indis = {
+        'I1': {
+            'Name': 'I1',
+            'Alive': True,
+            'Spouse': 'NA'
+        },
+        'I2': {
+            'Name': 'I2',
+            'Alive': True,
+            'Spouse': 'NA'
+        },
+        'I3': {
+            'Name': 'I3',
+            'Alive': True,
+            'Spouse': 'NA'
+        }}
+        self.assertEqual(listLivingSingle(indis), ['I1', 'I2', 'I3'])
+
+    def test2(self):
+        indis = {
+        'I1': {
+            'Name': 'I1',
+            'Alive': True,
+            'Spouse': 'NA'
+        },
+        'I2': {
+            'Name': 'I2',
+            'Alive': False,
+            'Spouse': 'NA'
+        },
+        'I3': {
+            'Name': 'I3',
+            'Alive': False,
+            'Spouse': 'NA'
+        }}
+        self.assertEqual(listLivingSingle(indis), ['I1'])
+
+    def test3(self):
+        indis = {
+        'I1': {
+            'Name': 'I1',
+            'Alive': True,
+            'Spouse': 'Blah'
+        },
+        'I2': {
+            'Name': 'I2',
+            'Alive': False,
+            'Spouse': 'NA'
+        },
+        'I3': {
+            'Name': 'I3',
+            'Alive': False,
+            'Spouse': 'NA'
+        }}
+        self.assertEqual(listLivingSingle(indis), [])
+
+if __name__ == '__main__':
+    unittest.main()
